cb8234fa71ce6e17a9feb6749848930a6c2dd4f0
sosena2 <149303452+sosena2@users.noreply.github.com>
2025-09-30 07:38:26 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (26.04%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,17 @@
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-    #   buttom up
+        # buttom up
+        dp = [[0]*n for _ in range(m)]
+        # base case
+        dp[m-1][n-1] = 1
 
-    # the last row
-       row = [1]*n
-       for i in range(m-1):
-           new = [1]*n
-           for j in range(n-2, -1, -1):
-                # summing the buttom and the right row
-               new[j] = new[j+1] + row[j]
-            # updating row
-           row = new
-       return row[0]
-
-
-
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                # base case
+                if i == m-1 and j == n-1:
+                    continue
+                down = dp[i+1][j] if i+1 < m else 0
+                right = dp[i][j+1] if j + 1 < n else 0
+                dp[i][j] = down  + right
+        return dp[0][0]
