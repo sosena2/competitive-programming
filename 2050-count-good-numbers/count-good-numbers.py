108645d0c5a34db732fc18cd98dfacfefc2cc055
sosena2 <149303452+sosena2@users.noreply.github.com>
2025-11-06 19:19:08 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.79%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+        
+        even = (n + 1) // 2  
+        odd = n // 2         
+
+        ans = pow(5, even, MOD) * pow(4, odd, MOD)
+        return ans % MOD
+      
+
+        
