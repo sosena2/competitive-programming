edf7d531403dfa48493fd1370da91a1f5bd1fab2
sosena2 <149303452+sosena2@users.noreply.github.com>
2026-04-17 18:21:45 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (45.75%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,7 +5,7 @@ class Solution:
         
         while low <= high:
             mid = (low + high) // 2
-            print(mid)
+    
             if target == nums[mid]:
                 return mid
             elif target > nums[mid]:
